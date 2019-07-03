1e9f36304be2e369cbbd6e74c3e63afc23c008f7
Leonardo Lima <leonardo1230987@gmail.com>
2019-07-03 03:15:23 -0400

    Funcionando perfeitamente no localhost

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const init = async () => {
 
     const server = Hapi.server({
         port: process.env.PORT || 3000,
-        host: 'localhost'
+        host: 'https://engetec-api.herokuapp.com'
     });
 
     server.route({
